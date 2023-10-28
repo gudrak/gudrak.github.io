521f08564855152fae163645f0a1c9f2c0e3325c
gudrak <63885769+zionserver@users.noreply.github.com>
2023-10-28 20:55:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Obtén elementos del DOM
-  var videoPlayer = document.getElementById("m3u-player");
-  var videoSource = document.getElementById("video-source");
+document.addEventListener('DOMContentLoaded', function() {
+    const player = document.getElementById('m3u-player');
+    const videoSource = document.getElementById('video-source');
+    
+    // Array de enlaces a videos en el archivo M3U
+    const videoUrls = [
+        "https://master.dl.sourceforge.net/project/confurtv/TBBT/T1/TBBT-T1-E3.mp4?viasf=1"
+        // Agrega aquí los demás enlaces de tu archivo index.m3u
+    ];
+    
+    let currentIndex = 0;
 
-  // Array de enlaces a videos en el archivo M3U
-  var videoUrls = [
-    "https://ia803106.us.archive.org/3/items/tbbt-t-1-e-2/TBBT-T1-E2.mp4",
-    "https://ia600509.us.archive.org/22/items/tbbt-t1-e3/TBBT-T1-E3.mp4"
-  ];
+    // Función para cambiar el video actual
+    function changeVideo() {
+        videoSource.src = videoUrls[currentIndex];
+        player.load();
+        player.play();
+        currentIndex = (currentIndex + 1) % videoUrls.length;
+    }
 
-  // Función para cambiar el video actual
-  function changeVideo(index) {
-    videoSource.setAttribute("src", videoUrls[index]);
-    videoPlayer.load(); // Recargar el reproductor de video
-    videoPlayer.play(); // Iniciar la reproducción automáticamente
-  }
+    // Inicializa con el primer video
+    changeVideo();
 
-  // Inicializar con el primer video
-  changeVideo(0);
-
-  // Escuchar eventos de cambio de video
-  videoPlayer.onended = function () {
-    // Cambia al siguiente video cuando el actual termine
-    var currentIndex = videoUrls.indexOf(videoSource.getAttribute("src"));
-    var nextIndex = (currentIndex + 1) % videoUrls.length;
-    changeVideo(nextIndex);
-  };
+    // Escucha el evento 'ended' para cambiar al siguiente video al finalizar
+    player.addEventListener('ended', function() {
+        changeVideo();
+    });
 });
