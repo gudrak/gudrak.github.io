92f5d2927ef3320d17a0e298623d4e4c57605731
gudrak <63885769+zionserver@users.noreply.github.com>
2023-10-28 20:08:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const player = document.getElementById('m3u-player');
-    const videoSource = document.getElementById('video-source');
-    
-    fetch('https://raw.githubusercontent.com/gudrak/gudrak.github.io/main/index.m3u')
-        .then(response => response.text())
-        .then(data => {
-            const lines = data.split('\n');
-            const videoUrl = lines.find(line => line.trim() !== '' && !line.startsWith('#'));
-            if (videoUrl) {
-                videoSource.src = videoUrl;
-                player.load();
-            }
-        })
-        .catch(error => console.error('Error loading M3U file:', error));
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén elementos del DOM
+  var videoPlayer = document.getElementById("m3u-player");
+  var videoSource = document.getElementById("video-source");
+
+  // Array de enlaces a videos en el archivo M3U
+  var videoUrls = [
+    "https://ia803106.us.archive.org/3/items/tbbt-t-1-e-2/TBBT-T1-E2.mp4",
+    "https://ia600509.us.archive.org/22/items/tbbt-t1-e3/TBBT-T1-E3.mp4"
+  ];
+
+  // Función para cambiar el video actual
+  function changeVideo(index) {
+    videoSource.setAttribute("src", videoUrls[index]);
+    videoPlayer.load(); // Recargar el reproductor de video
+    videoPlayer.play(); // Iniciar la reproducción automáticamente
+  }
+
+  // Inicializar con el primer video
+  changeVideo(0);
+
+  // Escuchar eventos de cambio de video
+  videoPlayer.onended = function () {
+    // Cambia al siguiente video cuando el actual termine
+    var currentIndex = videoUrls.indexOf(videoSource.getAttribute("src"));
+    var nextIndex = (currentIndex + 1) % videoUrls.length;
+    changeVideo(nextIndex);
+  };
 });
